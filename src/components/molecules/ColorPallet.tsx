e2f49a182e0a6612b2157c9e88d4997338f3d232
sh-oon <jshyh1@naver.com>
2024-07-06 21:23:36 +0900

    feat / 태그 색상 변경 팔레트 및 기능 추가

---
+++ src/components/molecules/ColorPallet.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import React, {} from 'react';
+import { css, useTheme } from '@emotion/react';
+import { TColor } from '@/types/theme';
+import { useDevice } from '@/context/DeviceContext';
+
+export type TPosition = {
+  x: number;
+  y: number;
+}
+
+type Props = {
+  position: TPosition;
+  colors: TColor[];
+  currentColor?: TColor;
+  onSelect: (color: TColor) => void;
+  availableColors: string[];
+}
+
+const ColorPallet = (
+  {
+    position,
+    colors,
+    currentColor,
+    onSelect,
+    availableColors,
+  }: Props) => {
+  console.log(currentColor);
+  const theme = useTheme();
+  const { isMobile } = useDevice();
+
+  return (
+    <div css={css`
+      position: fixed;
+      top: ${position.y + 9}px;
+      left: ${position.x}px;
+      background: ${theme.colors.grey[800]};
+      transform: ${isMobile ? null : 'translate(-50%, 0)'};
+      display: flex;
+      padding: 12px;
+      gap: 6px;
+      border-radius: 8px;
+    `}>
+      {
+        colors.map((color, index) => {
+          return (
+            <button
+              key={index}
+              css={css`
+                position: relative;
+                width: ${isMobile ? '24px' : '16px'};
+                height: ${isMobile ? '24px' : '16px'};
+                border-radius: 50%;
+                background: ${color}${!availableColors.includes(color) ? '80' : ''};
+                border: 2px solid ${currentColor === color ? theme.colors.grey[0] : theme.colors.grey[800]};
+              `}
+              onClick={() => onSelect(color)}
+              disabled={!availableColors.includes(color || '')}
+            >
+              {/* disabled 일 때 보이는 대각선*/}
+              {
+                !availableColors.includes(color) && (
+                  isMobile ? (
+                      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect y="19" width="27" height="1.5" rx="0.75" transform="rotate(-45 0 19)" fill="#E43333" />
+                      </svg>
+
+                    ) :
+                    (
+                      <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect y="13" width="18" height="1" rx="0.5" transform="rotate(-45 0 13)" fill="#E43333" />
+                      </svg>
+                    )
+                )
+              }
+            </button>
+          );
+        })
+      }
+    </div>
+  );
+};
+
+export default ColorPallet;
--- src/components/organisms/profile/ProfileSettingStep3.tsx
+++ src/components/organisms/profile/ProfileSettingStep3.tsx
@@ -7,143 +7,223 @@ import { useDevice } from '@/context/DeviceContext';
 import { useRecoilState } from 'recoil';
 import { thirdStep } from '@/recoil/store';
 import InputCharacterCounterItem from '@/components/atoms/input/InputCharacterCouterItem';
-import ColoredTag from '@/components/molecules/ColoredTag';
+import ColoredTag from '@/components/organisms/ColoredTag';
 import { SortableList } from '@/lib/dnd/SortableList';
+import { TColor } from '@/types/theme';
+import ColorPallet from '@/components/molecules/ColorPallet';
+import { Nullable } from '@/types/global';
 
 export type TItem = {
   id: string;
+  code: TColor;
   label: string;
 }
 
-const allColors = ['yellow', 'sky', 'green', 'pink', 'purple'];
 const ProfileSettingStep3 = () => {
   const theme = useTheme();
+  const secondaryColors: TItem[] =
+    Object.entries(theme.colors.secondary).map(([key, value]) => {
+      return { id: key, code: value, label: '' };
+    });
+
   const { isMobile } = useDevice();
   const [param, setParam] = useRecoilState(thirdStep);
   const [items, setItems] = useState<TItem[]>([]);
-  const [availableColors, setAvailableColors] = useState<string[]>(allColors.filter((color) => !items.some((item) => item.id === color)));
+  const [availableColors, setAvailableColors] = useState<TItem[]>(secondaryColors);
   const [currentItem, setCurrentItem] = useState<TItem>({
-    id: availableColors[0],
+    id: availableColors[0].id,
+    code: availableColors[0].code,
     label: '',
   });
+  const [isOpenColorPallet, setIsOpenColorPallet] = useState(false);
+  const [palletPosition, setPalletPosition] = useState({ x: 0, y: 0 });
+  const [activeItem, setActiveItem] = useState<Nullable<TItem>>(null);
 
-  const newAvailableColors = useMemo(() => allColors.filter((color) => !items.some((item) => item.id === color)), [items]);
+
+  const newAvailableColors =
+    useMemo(() =>
+      secondaryColors.filter((color: TItem) => !items.some((item) => item.id === color.id)), [items]);
 
   useEffect(() => {
     setAvailableColors(newAvailableColors);
-    setParam({ ...param, petFavs: items.map((item) => item.label) })
-  }, [items, newAvailableColors]);
+    setParam({ ...param, petFavs: items.map((item) => item.label) });
+  }, [items, newAvailableColors, currentItem.code]);
 
   useEffect(() => {
-    setCurrentItem({ id: availableColors[0], label: '' });
+    setCurrentItem({ id: availableColors[0].id, code: availableColors[0].code, label: '' });
   }, [availableColors]);
+
+  const getCurrentItem = (currentColor: TColor) => {
+    // items에 있는 아이템 중 현재 선택된 아이템을 찾아서 반환
+    const current = items.find((item) => item.code === currentColor);
+    if (current) {
+      return current;
+    } else {
+      // 전체에서 검색
+      const res = secondaryColors.find((item) => item.code === currentColor);
+      if (res) {
+        return res;
+      } else {
+        return null;
+      }
+    }
+
+  };
+
+  const openPalletHandler = (target: HTMLButtonElement, currentColor: TColor) => {
+    const rect = target.getBoundingClientRect();
+    const position = isMobile ? { x: rect.left - 14, y: rect.bottom } : {
+      x: rect.left + rect.width / 2,
+      y: rect.bottom,
+    };
+    if (!isOpenColorPallet) {
+      setPalletPosition(position);
+      setActiveItem(getCurrentItem(currentColor));
+      setIsOpenColorPallet(!isOpenColorPallet);
+    } else {
+      console.log(position, palletPosition);
+      if(position.x !== palletPosition.x || position.y !== palletPosition.y) {
+        setPalletPosition(position);
+      } else {
+        setActiveItem(null);
+        setIsOpenColorPallet(!isOpenColorPallet);
+      }
+    }
+  };
+
+  const selectColorHandler = (color: TColor) => {
+    if (items.includes(activeItem as TItem)) {
+      const target = items.find((item) => item === activeItem);
+      const newItem = secondaryColors.find((item) => item.code === color);
+      if (newItem) {
+        target!.code = newItem.code;
+        target!.id = newItem.id;
+      }
+      setItems(prev => {
+        if (target) {
+          prev[prev.indexOf(target)] = target;
+        }
+        return [...prev];
+      });
+    } else {
+      const newItem = getCurrentItem(color);
+      if (newItem) {
+        setCurrentItem(newItem);
+      }
+    }
+  };
 
 
   return (
-    <div css={css`
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      padding: 0 60px;
-      gap: 2rem;
-      @media ${theme.device.mobile} {
-        padding: 0;
-      }
-    `}>
+    <>
       <div css={css`
+        width: 100%;
         display: flex;
         flex-direction: column;
-        gap: 0.25rem;
+        padding: 0 60px;
+        gap: 2rem;
+        @media ${theme.device.mobile} {
+          padding: 0;
+        }
       `}>
-        <ThemedText type={isMobile ? 'labelMedium' : 'labelLarge'}>소개</ThemedText>
-        <div css={css`
-          display: flex;
-          gap: 0.5rem;
-        `}>
-          <InputCharacterCounterItem
-            value={param.petDesc}
-            validate={true}
-            setValue={(e: string) => setParam({ ...param, petDesc: e })}
-            maxLength={90}
-            placeholder="소개글을 입력해주세요"
-          />
-        </div>
-      </div>
-      <div css={css`
-        display: flex;
-        flex-direction: column;
-        gap: 0.75rem;
-      `}>
-        <div>
-          <ThemedText type={isMobile ? 'labelLarge' : 'labelLarge'}>좋아하는 것 </ThemedText>
-          <ThemedText type={isMobile ? 'labelLarge' : 'labelLarge'} css={css`color: ${theme.colors.grey[500]}`}>(최대
-            3개)</ThemedText>
-        </div>
-
-        <SortableList
-          id="sortable-list"
-          items={items}
-          onChange={setItems}
-          renderItem={(item) => (
-            <SortableList.Item id={item.id}>
-              <SortableList.DragHandle />
-              <ColoredTag
-                id={item.id}
-                color={theme.colors.secondary[item.id]}
-                setColor={(color) => {
-                  setItems((items) => {
-                    const index = items.findIndex((item) => item.id === item.id);
-                    const newItems = [...items];
-                    newItems[index] = { ...newItems[index], id: color };
-                    return newItems;
-                  });
-                }}
-                label={item.label}
-                mode={'view'}
-                removeHandler={(id) => {
-                  setItems((items) => items.filter((item) => item.id !== id));
-                }}
-              />
-            </SortableList.Item>
-          )}
-        />
-
-        {items.length < 3 && (
-          <ColoredTag
-            id={currentItem.id}
-            mode={'edit'}
-            color={theme.colors.secondary[currentItem.id]}
-            setColor={(color) => {
-              setItems((items) => {
-                const index = items.findIndex((item) => item.id === currentItem.id);
-                const newItems = [...items];
-                newItems[index] = { ...newItems[index], id: color };
-                return newItems;
-              });
-            }}
-            label={items.find((item) => item.id === currentItem.id)?.label || ''}
-            onBlur={(item) => {
-              if (item.label === '') return;
-              setItems((items) => [...items, item]);
-            }}
-          />
-        )}
-
         <div css={css`
           display: flex;
           flex-direction: column;
-          word-break: keep-all;
-          padding: 0.5rem 0.75rem;
-          background: ${theme.colors.grey[100]};
+          gap: 0.25rem;
         `}>
-          <ThemedText type={'captionLarge'} cssStyle={css`color: ${theme.colors.grey[600]}`}>이런 걸 좋아하나요?</ThemedText>
-          <ThemedText type={'captionLarge'} cssStyle={css`color: ${theme.colors.grey[400]}`}>
-            운동화끈_씹어먹기, 고구마말랭이, 낮잠자기, 꽃냄새_맡기, 성대모사장인,
-            목욕하기, 무한산책, 손바닥_위에서_꿀잠, 가만히_햇살_맞기
-          </ThemedText>
+          <ThemedText type={isMobile ? 'labelMedium' : 'labelLarge'}>소개</ThemedText>
+          <div css={css`
+            display: flex;
+            gap: 0.5rem;
+          `}>
+            <InputCharacterCounterItem
+              value={param.petDesc}
+              validate={true}
+              setValue={(e: string) => setParam({ ...param, petDesc: e })}
+              maxLength={90}
+              placeholder="소개글을 입력해주세요"
+            />
+          </div>
+        </div>
+        <div css={css`
+          display: flex;
+          flex-direction: column;
+          gap: 0.75rem;
+        `}>
+          <div>
+            <ThemedText type={isMobile ? 'labelLarge' : 'labelLarge'}>좋아하는 것 </ThemedText>
+            <ThemedText type={isMobile ? 'labelLarge' : 'labelLarge'} css={css`color: ${theme.colors.grey[500]}`}>(최대
+              3개)</ThemedText>
+          </div>
+
+          {items.length > 0 && (
+            <SortableList
+              id="sortable-list"
+              items={items}
+              onChange={setItems}
+              renderItem={(item) => (
+                <SortableList.Item id={item.id}>
+                  <SortableList.DragHandle />
+                  <>
+                    <ColoredTag
+                      id={item.id}
+                      color={theme.colors.secondary[item.id]}
+                      label={item.label}
+                      mode={'view'}
+                      removeHandler={(id) => {
+                        setItems((items) => items.filter((item) => item.id !== id));
+                      }}
+                      setOpen={openPalletHandler}
+                    />
+                  </>
+                </SortableList.Item>
+              )}
+            />
+          )}
+
+          {items.length < 3 && (
+            <ColoredTag
+              id={currentItem.id}
+              mode={'edit'}
+              color={theme.colors.secondary[currentItem.id]}
+              setOpen={openPalletHandler}
+              label={items.find((item) => item.id === currentItem.id)?.label || ''}
+              onBlur={(item) => {
+                if (item.label === '') return;
+                setItems((items) => [...items, item]);
+                if (isOpenColorPallet) setIsOpenColorPallet(!isOpenColorPallet);
+              }}
+            />
+          )}
+
+          <div css={css`
+            display: flex;
+            flex-direction: column;
+            word-break: keep-all;
+            padding: 0.5rem 0.75rem;
+            background: ${theme.colors.grey[100]};
+          `}>
+            <ThemedText type={'captionLarge'} cssStyle={css`color: ${theme.colors.grey[600]}`}>이런 걸 좋아하나요?</ThemedText>
+            <ThemedText type={'captionLarge'} cssStyle={css`color: ${theme.colors.grey[400]}`}>
+              운동화끈_씹어먹기, 고구마말랭이, 낮잠자기, 꽃냄새_맡기, 성대모사장인,
+              목욕하기, 무한산책, 손바닥_위에서_꿀잠, 가만히_햇살_맞기
+            </ThemedText>
+          </div>
         </div>
       </div>
-    </div>
+
+      {
+        isOpenColorPallet && (
+          <ColorPallet
+            position={palletPosition}
+            colors={secondaryColors.map((color) => color.code)}
+            currentColor={activeItem?.code}
+            onSelect={selectColorHandler}
+            availableColors={availableColors.map((color) => color.code)}
+          />
+        )
+      }
+    </>
   );
 };
 
