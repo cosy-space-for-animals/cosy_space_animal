35781e5c5fadcc1d9838bbebe8033bc5193ef9a0
nueizkk <nueizkk@gmail.com>
2024-07-09 11:50:34 +0900

    style / RoundButton.tsx disabled시, cursor 변경

--- src/components/atoms/buttons/RoundButton.tsx
+++ src/components/atoms/buttons/RoundButton.tsx
@@ -78,6 +78,9 @@ export default function RoundButton({
           text-align: center;
           border: none;
           outline: none;
+          &:disabled {
+            cursor: default;
+          }
         `,
         styles[type],
       )}
